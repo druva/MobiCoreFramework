2a5b08f0b277c8b0e506576a62171c9e0c855242
Druva Yarlagadda <druva.yarlagadda@valuelabs.com>
2018-04-26 19:31:48 -0700

    initial commit

---
+++ MobiCore-iOS/MobiCore_iOS.h
@@ -0,0 +1,19 @@
+//
+//  MobiCore_iOS.h
+//  MobiCore-iOS
+//
+//  Created by Druva Yarlagadda on 4/26/18.
+//  Copyright © 2018 Druva Yarlagadda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobiCore_iOS.
+FOUNDATION_EXPORT double MobiCore_iOSVersionNumber;
+
+//! Project version string for MobiCore_iOS.
+FOUNDATION_EXPORT const unsigned char MobiCore_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobiCore_iOS/PublicHeader.h>
+
+
---
+++ MobiCore-tvOS/MobiCore_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  MobiCore_tvOS.h
+//  MobiCore-tvOS
+//
+//  Created by Druva Yarlagadda on 4/26/18.
+//  Copyright © 2018 Druva Yarlagadda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MobiCore_tvOS.
+FOUNDATION_EXPORT double MobiCore_tvOSVersionNumber;
+
+//! Project version string for MobiCore_tvOS.
+FOUNDATION_EXPORT const unsigned char MobiCore_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobiCore_tvOS/PublicHeader.h>
+
+
